a9be4334bde1febb5b376218664e082c9cb3b77f
eworsham <eworsham64@gmail.com>
2024-12-05 19:09:10 -0500

    Added authentication through login and logout endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
+const passport = require('passport')
 
 // router.use('/paintings', require('./paintingsRoute'));
 // router.use('/sculptures', require('./sculpturesRoute'));
 // router.use('/stores', require('./storesRoute'));
 // router.use('/users', require('./usersRoute'));
+
+router.get('/login', passport.authenticate('github'), (req, res) => {})
+
+router.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        return next(err)
+    })
+    res.redirect('/')
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 const swaggerDocumentLocalhost = require('./swagger-output-localhost.json');
 const mongodb = require('./data/database');
+const passport = require('passport')
+const session = require('express-session')
+const GitHubStrategy = require('passport-github2').Strategy
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -17,6 +20,55 @@ if (process.env.ENVIRONMENT == 'dev') {
   app.use('/api-docs', swaggerUi.serve);
   app.use('/api-docs', swaggerUi.setup(swaggerDocument));
 }
+
+// Authentication Middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: true
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get('/', (req, res) => {
+  res.send(
+    req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Logged Out'
+  );
+});
+
+app.get(
+  '/github/callback',
+  passport.authenticate('github', {
+    failureRedirect: '/api-docs',
+    session: false
+  }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.redirect('/');
+  }
+);
 
 // Routes
 app.use('/', require('./routes'));
